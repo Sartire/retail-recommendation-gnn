b38e002745087b42e9ab5dc2e973ab4d7bffbb07
Sartire <alexvdeluca@gmail.com>
2025-11-24 16:00:03 -0500

    add module imports

--- run_batching.sh
+++ run_batching.sh
@@ -16,6 +16,9 @@
 cd /scratch/mcg4aw/retail-recommendation-gnn
 
 module purge
+module load gcc/11.4.0
+module load openmpi/4.1.4
+module load apptainer/1.3.4
 module load python/3.11.4
 module load pytorch/2.7.0
 
