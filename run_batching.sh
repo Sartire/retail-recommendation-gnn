d0db85a253cd10fbdd383cc8ad638b5ada27a35a
Sartire <alexvdeluca@gmail.com>
2025-11-24 15:22:32 -0500

    Create run_batching.sh

---
+++ run_batching.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1  
+#SBATCH --cpus-per-task=20         
+#SBATCH --mem-per-cpu=6000            
+#SBATCH --time=01:00:00   
+#SBATCH --chdir=/scratch/mcg4aw/retail-recommendation-gnn
+#SBATCH --output= /scratch/mcg4aw/slurm_logs/output_%j.txt
+#SBATCH --mail-user=mcg4aw@virginia.edu
+#SBATCH --mail-type=ALL
+
+
+python preprocess_batches.py $SLURM_CPUS_PER_TASK
