2652d7cac0fe867518bf95d125b80573ec7f446c
Sartire <alexvdeluca@gmail.com>
2025-11-24 15:51:58 -0500

    reduce num requested cpus

--- run_batching.sh
+++ run_batching.sh
@@ -4,13 +4,14 @@
 #SBATCH --time=1:00:00   # job time limit
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=1   # number of tasks per node
-#SBATCH --cpus-per-task=20   # number of CPU cores per task
+#SBATCH --cpus-per-task=10   # number of CPU cores per task
 #SBATCH --partition=standard   # partition
 #SBATCH -J "CreateRetailBatches"   # job name
 #SBATCH --mail-user=mcg4aw@virginia.edu   # email address
 #SBATCH --mail-type=ALL
 #SBATCH --constraint=rivanna   # cluster
 #SBATCH --account=shakeri_ds6050   # allocation name
+#SBATCH --mem=6G
 
 cd /scratch/mcg4aw/retail-recommendation-gnn
 
@@ -20,4 +21,4 @@ module load pytorch/2.7.0
 
 pip install torch_geometric
 
-python preprocess_batches.py --num_workers 20
+python preprocess_batches.py --num_workers 10
